3b40b89d5526d03ce10453440985e92363868961
Yorran Carvalho <yorrancarvalho18@gmail.com>
2023-04-19 23:47:49 -0300

    main finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import {Home as  HomeIcon, Search, Library} from 'lucide-react'
+import {Home as  HomeIcon, Search, Library, ChevronLeftSquare, ChevronLeft, ChevronRight, Play} from 'lucide-react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -40,7 +41,92 @@ export default function Home() {
 
       </aside>
       <main className="flex-1 p-6">
-        main
+        <div className='flex items-center gap-4'>
+          <button className='rounded-full bg-black/40 p-1'>
+            <ChevronLeft/>
+          </button>
+          <button className='rounded-full bg-black/40 p-1'>
+            <ChevronRight/>
+          </button>
+        </div>
+
+        <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
+
+        <div className='grid grid-cols-3 gap-4 nt-4'>
+          <a href='#' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src='/album.jpg' width={104} height={104} alt='Capa do Albúm Meteora'/>
+            <strong>METEORA</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play/>
+            </button>
+          </a>
+          <a href='#' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src='/album.jpg' width={104} height={104} alt='Capa do Albúm Meteora'/>
+            <strong>METEORA</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play/>
+            </button>
+          </a>
+          <a href='#' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src='/album.jpg' width={104} height={104} alt='Capa do Albúm Meteora'/>
+            <strong>METEORA</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play/>
+            </button>
+          </a>
+          <a href='#' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src='/album.jpg' width={104} height={104} alt='Capa do Albúm Meteora'/>
+            <strong>METEORA</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play/>
+            </button>
+          </a>
+          <a href='#' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src='/album.jpg' width={104} height={104} alt='Capa do Albúm Meteora'/>
+            <strong>METEORA</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play/>
+            </button>
+          </a>
+          <a href='#' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src='/album.jpg' width={104} height={104} alt='Capa do Albúm Meteora'/>
+            <strong>METEORA</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play/>
+            </button>
+          </a>
+        </div>
+
+        <h2 className='font-semibold text-2xl mt-10'>Made for Vlad Țepeș</h2>
+
+        <div className='grid grid-cols-5 gap-4 mt-4'>
+        <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src='/album.jpg' className='w-full' width={120} height={120} alt='Capa do Albúm Meteora'/>
+          <strong className='font-semibold'>Daily Mix 1</strong>
+          <span className='text-sm text-zinc-400'>Dragonforce, BabyMetal and System of a Down</span>
+        </a>
+        <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src='/album.jpg' className='w-full' width={120} height={120} alt='Capa do Albúm Meteora'/>
+          <strong className='font-semibold'>Daily Mix 1</strong>
+          <span className='text-sm text-zinc-400'>Dragonforce, BabyMetal and System of a Down</span>
+        </a>
+        <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src='/album.jpg' className='w-full' width={120} height={120} alt='Capa do Albúm Meteora'/>
+          <strong className='font-semibold'>Daily Mix 1</strong>
+          <span className='text-sm text-zinc-400'>Dragonforce, BabyMetal and System of a Down</span>
+        </a>
+        <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src='/album.jpg' className='w-full' width={120} height={120} alt='Capa do Albúm Meteora'/>
+          <strong className='font-semibold'>Daily Mix 1</strong>
+          <span className='text-sm text-zinc-400'>Dragonforce, BabyMetal and System of a Down</span>
+        </a>
+        <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src='/album.jpg' className='w-full' width={120} height={120} alt='Capa do Albúm Meteora'/>
+          <strong className='font-semibold'>Daily Mix 1</strong>
+          <span className='text-sm text-zinc-400'>Dragonforce, BabyMetal and System of a Down</span>
+        </a>
+        </div>
+
       </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
