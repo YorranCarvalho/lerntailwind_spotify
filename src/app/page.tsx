20629e9431bc60ed1f7c7015b9886be79d3d094f
Yorran Carvalho <yorrancarvalho18@gmail.com>
2023-04-19 21:53:16 -0300

    navbar finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
       <aside className="w-72 bg-zinc-950 p-6">
-        <nav className='space-y-5'>
+        <div className='flex items-center gap-2'>
+          <div className='w-3 h-3 bg-red-500 rounded-full'/>
+          <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
+          <div className='w-3 h-3 bg-green-500 rounded-full'/>
+        </div>
+        <nav className='space-y-5 mt-5'>
           <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
             <HomeIcon/>
             Home
@@ -19,6 +24,20 @@ export default function Home() {
             Your Library
           </a>
         </nav>
+
+        <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Heavy Metal</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Sofrência</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Rock doido</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>LoLzin</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Ouvir enquanto programo</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Rap de Anime</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Babymetal</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Dragonforce</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Ghost</a>
+          <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Linkin Park</a>
+        </nav>
+
       </aside>
       <main className="flex-1 p-6">
         main
